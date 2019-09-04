e595c7684f770f6a7ea872d1f6b2439bbaa5ed85
Jamie Lentin <jm@lentin.co.uk>
2019-09-04 16:22:28 +0100

    ut_cmp_*: Graduate from example to included function

---
+++ R/ut_cmp.R
@@ -0,0 +1,108 @@
+# Return TRUE if all.equal(a, b), otherwise show the difference with str().
+# like all.equal but with more diagnostic output
+ut_cmp_equal <- function(a, b, filter = NULL, ...) {
+    cmp_inner(a, b, comparison_fn = function (x, y) all.equal(x, y, ...), filter = filter)
+}
+
+# Same as ut_cmp_equal(), but uses identical instead
+ut_cmp_identical <- function(a, b, filter = NULL) {
+    cmp_inner(a, b, comparison_fn = identical, filter = filter)
+}
+
+cmp_inner <- function(a, b, comparison_fn = all.equal, filter = NULL) {
+    # Compare inputs, if equal then we're done
+    ae_output <- comparison_fn(a, b)
+    if (isTRUE(ae_output)) return(TRUE)
+    if (isFALSE(ae_output)) ae_output <- c()
+
+    # Try each filter function in turn, until we find one that produces useful
+    # diff output
+    for (f in list(
+            # Add any custom filtering function
+            filter,
+            # Strings can be compared directly using writeLines
+            ifelse(is.character(a) && is.character(b), writeLines, 'ignore'),
+            # print will pick up any generics defined for custom types
+            print,
+            # Fall back to parsing with str
+            function (x) utils::str(x, vec.len = 1000, digits.d = 5, nchar.max = 1000),
+            NULL)) {
+        if (is.function(f)) {
+            diff_lines <- output_diff(f(a), f(b))
+            if (length(diff_lines) > 0) {
+                break
+            }
+        }
+    }
+    diff_lines <- c(ae_output, diff_lines)
+
+    if (interactive() && sys.nframe() == 2) {
+        # Interactive and called at a top-level, so print the output nicely
+        writeLines(diff_lines)
+    }
+    invisible(diff_lines)
+}
+
+# Return location of git, separated so we can mock it
+git_binary <- function() {
+    Sys.which('git')
+}
+
+# Given 2 stdout-producing arguments, return human-readable word-diff lines, using git diff if available.
+output_diff <- function (a_out, b_out) {
+    path_join <- function(letter, path) {
+        # On UNIX a diff path will look like a/tmp/Rtmp...
+        out <- paste0(letter, if (substring(path, 1, 1) == '/') '' else '/', path)
+        if (grepl("\\", out, fixed = TRUE)) {
+            # On Windows a diff path will look like "a/D:\\temp\\Rtmp..."
+            out <- paste0('"', gsub("\\", "\\\\", out, fixed = TRUE), '"')
+        }
+        out
+    }
+
+    # Write 2 tempfiles, use git to compare
+    if (file.exists(git_binary())) {
+        a_path <- tempfile(pattern = "a.")
+        utils::capture.output(a_out, file = a_path)
+        on.exit(unlink(a_path))
+
+        b_path <- tempfile(pattern = "b.")
+        utils::capture.output(b_out, file = b_path)
+        on.exit(unlink(b_path))
+
+        out <- suppressWarnings(system2(Sys.which('git'), c(
+            "diff",
+            "--no-index",
+            "--color",
+            "--word-diff=plain",
+            "--minimal",
+            "-U100000000",  # Lines of context, assuming output is no longer than this
+            a_path,
+            b_path,
+            NULL), input = "", stdout = TRUE, stderr = TRUE))
+
+        # Remove useless git preamble
+        out <- grep("^(\033\\[.*?m)?(index|diff|@@) ", out, value = TRUE, invert = TRUE, perl = TRUE)
+
+        # Replace temp filenames with the expression cmp was called with
+        out <- gsub(path_join("a", a_path), deparse(sys.call(-2)[[2]], nlines = 1), out, fixed = TRUE)
+        out <- gsub(path_join("b", b_path), deparse(sys.call(-2)[[3]], nlines = 1), out, fixed = TRUE)
+
+        # Remove any trailing newline
+        if (length(out) > 0 && out[length(out)] == "") {
+            out <- head(out, -1)
+        }
+        return(out)
+    }
+
+    # Fallback: Capture the strings and compare that
+    a_repr <- utils::capture.output(a_out)
+    b_repr <- utils::capture.output(b_out)
+    if (identical(a_repr, b_repr)) {
+        return(c())
+    } else {
+        return(c(
+            paste0("--- ", deparse(sys.call(-2)[[2]], nlines = 1)), a_repr,
+            paste0("+++ ", deparse(sys.call(-2)[[3]], nlines = 1)), b_repr))
+    }
+}
---
+++ tests/test_ut_cmp.R
@@ -0,0 +1,122 @@
+library(unittest)
+
+# Compare character output of a (failing) cmp function, ignoring colors
+cmp_lines <- function (actual, ...) {
+    # Remove color escape codes
+    no_color <- gsub('\033\\[.*?m', '', actual, perl = TRUE)
+    if(identical(no_color, c(...)[!is.null(c(...))])) {
+        return(TRUE)
+    }
+    # utils::str(no_color, vec.len = 1000, digits.d = 5, nchar.max = 1000)
+    return(actual)
+}
+
+# Mock (fn) in namespace with (replacement) whilst (block) is being evaluated
+mock <- function (fn, replacement, block) {
+    # Get the name of the function from the unevaluated argument,
+    # assuming it's of the form package::name
+    fn_name <- as.character(as.list(sys.call()[[2]])[[3]])
+    ns <- environment(fn)
+
+    orig_fn <- get(fn_name, env = ns)
+    unlockBinding(fn_name, env = ns)
+    assign(fn_name, replacement, envir = ns)
+    on.exit(assign(fn_name, orig_fn, envir = ns), add = TRUE)
+
+    block
+}
+
+ok_group("ut_cmp_equal", (function () {
+    ok(isTRUE(ut_cmp_equal(4, 4)), "Identical objects return true")
+    ok(isTRUE(ut_cmp_equal(as.integer(4), 4)), "Equivalent objects return true (i.e. integer vs. number)")
+    ok(isTRUE(ut_cmp_equal(0.01, 0.02, tolerance = 0.1)), "Additional arguments passed through to all.equal")
+
+    if (!file.exists(unittest:::git_binary())) {
+        ok(TRUE, "# skip git not available")
+        return()
+    }
+
+    ok(cmp_lines(ut_cmp_equal(c(2,4,2,8), c(5,4,2,1)),
+        'Mean relative difference: 1',
+        '--- c(2, 4, 2, 8)',
+        '+++ c(5, 4, 2, 1)',
+        '[1] [-2-]{+5+} 4 2 [-8-]{+1+}',
+        NULL), "Vectors filtered by str, individual differences highlighted")
+
+    ok(!withVisible(ut_cmp_equal("apples", "oranges"))$visible,
+        "Output of comparision isn't visible (we should print it at a real console though)")
+
+    do_a_thing <- function (x) seq(x)
+    ok(cmp_lines(ut_cmp_equal(do_a_thing(4), do_a_thing(1 + 2)),
+        'Numeric: lengths (4, 3) differ',
+        '--- do_a_thing(4)',
+        '+++ do_a_thing(1 + 2)',
+        '[1] 1 2 3[-4-]',
+        NULL), "The ---/+++ lines show expressions handed to ut_cmp_equal()")
+
+    ok(cmp_lines(ut_cmp_equal(list(c(1, 2, 8), c(2, 3, 2), 10, 11, 12, 13), list(c(1, 2, 3), c(2, 3, 2), 10, 11, 12, 13)),
+        "Component 1: Mean relative difference: 0.625",
+        "--- list(c(1, 2, 8), c(2, 3, 2), 10, 11, 12, 13)",
+        "+++ list(c(1, 2, 3), c(2, 3, 2), 10, 11, 12, 13)",
+        "[[1]]",
+        "[1] 1 2 [-8-]{+3+}",
+        "",
+        "[[2]]",
+        "[1] 2 3 2",
+        "",
+        "[[3]]",
+        "[1] 10",
+        "",
+        "[[4]]",
+        "[1] 11",
+        "",
+        "[[5]]",
+        "[1] 12",
+        "",
+        "[[6]]",
+        "[1] 13",
+        NULL), "We return the whole file as context, not just the usual 3 lines")
+    
+    ok(cmp_lines(ut_cmp_equal(c("'Ouch!' he said,", "it was an iron bar."), c("Ooops!", "it was an accident.")),
+        '2 string mismatches',
+        '--- c("\'Ouch!\' he said,", "it was an iron bar.")',
+        '+++ c("Ooops!", "it was an accident.")',
+        "[-'Ouch!' he said,-]{+Ooops!+}",
+        'it was an [-iron bar.-]{+accident.+}',
+        NULL), "Character vectors get compared one per line")
+})())
+
+# Mock git_binary(), so we don't find git even if it is available
+ok_group("ut_cmp_equal:nogit", mock(unittest:::git_binary, function () "/not-here", {
+    ok(cmp_lines(ut_cmp_equal(c(2,4,2,8), c(5,4,2,1)),
+        'Mean relative difference: 1',
+        '--- c(2, 4, 2, 8)',
+        '[1] 2 4 2 8',
+        '+++ c(5, 4, 2, 1)',
+        '[1] 5 4 2 1',
+        NULL), "No git available, so show outputs side by side")
+}))
+
+ok_group("ut_cmp_identical", (function () {
+    if (!file.exists(unittest:::git_binary())) {
+        ok(TRUE, "# skip git not available")
+        return()
+    }
+
+    ok(isTRUE(ut_cmp_identical(4, 4)), "Identical objects return true")
+    ok(cmp_lines(ut_cmp_identical(as.integer(4), 4),
+        '--- as.integer(4)',
+        '+++ 4',
+        ' [-int-]{+num+} 4',
+        NULL), "Equivalent objects do not, unlike ut_cmp_equal(). We also fall back to using str(), as print() will produce identical output")
+})())
+
+ok_group("ut_cmp_identical:nogit", mock(unittest:::git_binary, function () "/not-here", {
+    ok(isTRUE(ut_cmp_identical(4, 4)), "Identical objects return true")
+    ok(cmp_lines(ut_cmp_identical(as.integer(4), 4),
+        '--- as.integer(4)',
+        ' int 4',
+        '+++ 4',
+        ' num 4',
+        NULL), "Equivalent objects do not, unlike ut_cmp_equal(). We also fall back to using str(), as print() will produce identical output")
+}))
