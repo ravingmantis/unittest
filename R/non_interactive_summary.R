575f6319a14d2474f57a300910e104012a1fd38a
Jamie Lentin <jm@lentin.co.uk>
2019-09-09 11:45:09 +0100

    non_interactive_summary: Bail out message on fatal script errors
    If a test setup crashed whilst in non-interactive mode, we used to display
    the error (via. the default error-handler), but then display an apparently
    successful final line "Looks like you passed all (x) tests".
    
    Instead, add an error handler capturing the error lines and showing them
    in proper TAP format. Include a traceback too, for good measure.

--- R/non_interactive_summary.R
+++ R/non_interactive_summary.R
@@ -20,7 +20,11 @@ non_interactive_exit <- function( e ) {
     if( exists('outcomes', where = e) && nrow(get('outcomes', pos = e)) ) {
          tests.total <- nrow(get('outcomes', pos = e))
          tests.failed <- sum(! get('outcomes', pos = e)$status) 
-         if (tests.failed) {
+         if ( exists('errors', where = e) ) {
+            tests.errors <- get('errors', pos = e)
+            writeLines(paste("Bail out! Looks like", tests.total, "tests passed, but script ended prematurely", collapse = " "))
+            writeLines(paste("#", tests.errors))
+         } else if (tests.failed) {
              cat(paste("# Looks like you failed", tests.failed, "of", tests.total, "tests.\n", collapse = " "))
              # We need to alter the status code, stop() doesn't work, not allowed to use .Last, should only happen as script is terminating anyway.
              quit(save = "no", status = 10, runLast=FALSE)
@@ -32,10 +36,33 @@ non_interactive_exit <- function( e ) {
     }
 }
 
+non_interactive_error_handler <- function() {
+    # Generate a traceback string
+    tb <- head(sys.calls(), -1)
+    tb <- lapply(seq_along(tb), function (i) {
+        c(
+            paste0(i, ":"),
+            deparse(tb[[i]], nlines = 3)
+        )
+    })
+
+    assign('errors', c(
+        unlist(strsplit_with_emptystr(geterrmessage(), "\n")),
+        "Traceback:", unlist(tb)
+    ), pos = pkg_vars)
+
+    q("no", status = 11, runLast = FALSE)
+}
+
 .onAttach <- function(libname, pkgname) {
     if (interactive()) return()
 
     reg.finalizer(pkg_vars, non_interactive_exit, onexit = TRUE)
+
+    # Only register our error handler if we'd use the default otherwise
+    if (is.null(getOption('error'))) {
+        options(error = non_interactive_error_handler)
+    }
 }
 
 # "Note that when an R session is finished, packages are not detached and namespaces are not unloaded, so the corresponding hooks will not be run." ?getHook
@@ -46,4 +73,5 @@ non_interactive_exit <- function( e ) {
     if (interactive()) return()
 
     rm('outcomes', pos = pkg_vars)
+    rm('errors', pos = pkg_vars)
 }
