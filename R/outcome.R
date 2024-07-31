c81a3f846ce85b6e18701758e69e2ed94a5d388c
Jamie Lentin <jm@lentin.co.uk>
2024-07-31 12:34:17 +0000

    outcome: Break up non_interactive_summary
    Extract the outcome summary part of non_interactive_summary and move it
    along with other outcome handling code into outcome.R
    
    The code (should be) functionally identical.

---
+++ R/outcome.R
@@ -0,0 +1,51 @@
+pkg_vars <- new.env(parent = emptyenv())
+
+# Clear out pkg_vars so any test failures aren't reported
+clear_outcomes <- function () {
+    if (exists('outcomes', where = pkg_vars)) rm('outcomes', pos = pkg_vars)
+    if (exists('errors', where = pkg_vars)) rm('errors', pos = pkg_vars)
+}
+
+assign_outcome <- function(outcome) {
+    if (interactive()) return()
+    # as per assign() invoked for side effect
+    if( ! exists('outcomes', where = pkg_vars) ) {
+        assign(
+            'outcomes',
+             data.frame(status = logical(0), description = character(0), output = character(0), stringsAsFactors = FALSE),
+             pos = pkg_vars
+        )
+    }
+    assign('outcomes', rbind(get('outcomes', pos = pkg_vars), outcome), pos = pkg_vars)
+}
+
+# Output human-readable version of summary contents, return number of failing tests, or -1 if an error occured
+outcome_summary <- function () {
+    outcomes <- if (exists('outcomes', where = pkg_vars)) get('outcomes', pos = pkg_vars) else data.frame(status = logical(0))
+    tests.total <- nrow(outcomes)
+    tests.failed <- sum(!outcomes$status)
+    errors <- if (exists('errors', where = pkg_vars)) get('errors', pos = pkg_vars) else NULL
+
+    if( nrow(outcomes) > 0 ) {
+         if ( !is.null(errors) ) {
+             write_ut_lines(
+                 paste("Bail out! Looks like", tests.total, "tests passed, but script ended prematurely", collapse = " "),
+                 paste("#", errors),
+                 NULL)
+             tests.failed <- -1
+         } else if (tests.failed) {
+             write_ut_lines(
+                 paste("# Looks like you failed", tests.failed, "of", tests.total, "tests.", collapse = " "),
+                 if (tests.failed != tests.total && tests.failed < 20) {
+                     paste0("# ", which(!outcomes$status), ": ", outcomes[!outcomes$status, "description"])
+                 },
+                 NULL)
+         } else {
+             write_ut_lines(
+                 paste("# Looks like you passed all", tests.total, "tests.", collapse = " "),
+                 NULL)
+         }
+    }
+
+    return(invisible(tests.failed))
+}
---
+++ R/zzz_non_interactive.R
@@ -0,0 +1,49 @@
+# everything should no-op in interactive session
+
+non_interactive_exit <- function( e ) {
+    tests.failed <- outcome_summary()
+
+    if (tests.failed > 0) {
+        # We need to alter the status code, stop() doesn't work, not allowed to use .Last, should only happen as script is terminating anyway.
+        quit(save = "no", status = 10, runLast=FALSE)
+    }
+}
+
+non_interactive_error_handler <- function() {
+    # Generate a traceback string
+    tb <- head(sys.calls(), -1)
+    tb <- lapply(seq_along(tb), function (i) {
+        c(
+            paste0(i, ":"),
+            deparse(tb[[i]], nlines = 3)
+        )
+    })
+
+    assign('errors', c(
+        unlist(strsplit_with_emptystr(geterrmessage(), "\n")),
+        "Traceback:", unlist(tb)
+    ), pos = pkg_vars)
+
+    q("no", status = 11, runLast = FALSE)
+}
+
+.onAttach <- function(libname, pkgname) {
+    if (interactive()) return()
+
+    reg.finalizer(pkg_vars, non_interactive_exit, onexit = TRUE)
+
+    # Only register our error handler if we'd use the default otherwise
+    if (is.null(getOption('error'))) {
+        options(error = non_interactive_error_handler)
+    }
+}
+
+# "Note that when an R session is finished, packages are not detached and namespaces are not unloaded, so the corresponding hooks will not be run." ?getHook
+# so we can assume that if this is called then the package is being detached (and maybe also unloaded)
+# the assumption is that if the package is also unloaded that any reg.finalizers would be run some time later when garbage collection happens
+# So the onDetach event allows us to clear out the pkg_vars environment so that when the finalizer is called the non_interactive_exit will no-op
+.onDetach <- function(libpath) {
+    if (interactive()) return()
+
+    clear_outcomes()
+}
